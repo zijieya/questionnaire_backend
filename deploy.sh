6252000dba0c1f7bf18e2f31baabbd1ce1cd9cdb
zijieya <1347133477@qq.com>
2018-12-10 18:47:26 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #清理文件
 ssh  $testserver <<EOF
- cd  www/wwwroot/questionnairetest_jieblog_win
+ cd  /www/wwwroot/questionnairetest_jieblog_win
   rm -rf *
 EOF
 #复制文件
-scp build/libs/*.war  $testserver:www/wwwroot/questionnairetest_jieblog_win/
+scp build/libs/*.war  $testserver:/www/wwwroot/questionnairetest_jieblog_win/
 #运行
 ssh  $testserver << EOF
  jar -xf *.war
