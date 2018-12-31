3c1adb5d0f1276f94df2ab5b3f8b98fb18dfb369
zijieya <1347133477@qq.com>
2018-12-31 21:42:42 +0800

    增加分页总数

--- src/main/java/win/jieblog/questionnaire/model/contract/user/GlobalSearchForUserResponse.java
+++ src/main/java/win/jieblog/questionnaire/model/contract/user/GlobalSearchForUserResponse.java
@@ -1,10 +1,13 @@
 package win.jieblog.questionnaire.model.contract.user;
 
+import io.swagger.annotations.ApiModelProperty;
 import win.jieblog.questionnaire.model.contract.BaseResponse;
 
 import java.util.List;
 
 public class GlobalSearchForUserResponse extends BaseResponse {
+    @ApiModelProperty(value = "分页总数")
+    private long total;
  private List<ServeyItem> list;
 
     public List<ServeyItem> getList() {
@@ -13,5 +16,13 @@ public class GlobalSearchForUserResponse extends BaseResponse {
 
     public void setList(List<ServeyItem> list) {
         this.list = list;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
     }
 }
