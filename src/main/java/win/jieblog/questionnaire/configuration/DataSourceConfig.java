8b68f20fd6cba70a742b8936859d86cf252f090a
zijieya <1347133477@qq.com>
2019-01-19 15:19:34 +0800

    配置两个datasource

---
+++ src/main/java/win/jieblog/questionnaire/configuration/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package win.jieblog.questionnaire.configuration;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.one")
+    public DataSource dataSourceOne(){
+        return DruidDataSourceBuilder.create().build();
+    }
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid.two")
+    public DataSource dataSourceTwo(){
+        return DruidDataSourceBuilder.create().build();
+    }
+}
